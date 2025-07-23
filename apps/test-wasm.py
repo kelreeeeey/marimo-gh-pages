fb1be17efbb5cecd1903f722fef5920a06c56fc5
kelreeeeey <kelreytaufiq08@mail.ugm.ac.id>
2025-07-23 20:28:33 +0700

    update app notebook

--- apps/test-wasm.py
+++ apps/test-wasm.py
@@ -1,10 +1,10 @@
 import marimo
 
 __generated_with = "0.14.12"
-app = marimo.App(width="full")
+app = marimo.App(width="columns")
 
 
-@app.cell
+@app.cell(column=0)
 def _():
     import marimo as mo
     return (mo,)
@@ -15,9 +15,14 @@ def _(mo):
     slider = mo.ui.slider(1, 10, 1, label="")
     return (slider,)
 
-
 @app.cell
-def _(mo, slider):
+def _(mo):
+    number = mo.ui.number(1, 10, 1, label="")
+    return (number)
+
+
+@app.cell(column=1)
+def _(mo, slider, number):
     mo.md(
         f"""
         marimo is a **reactive** Python notebook.
@@ -25,9 +30,28 @@ def _(mo, slider):
         Slide me, dude { slider }
 
         { "##" + "🍃" * slider.value }
+
+        How about {number}
+
+        { "##" + "🥲" * number.value }
         """)
     return
 
+@app.cell(column=2)
+def _(mo, slider, number):
+    mo.md(
+        f"""
+        marimo is a **reactive** Python notebook.
+
+        Slide me, dude { slider }
+
+        { "##" + "🍃" * slider.value }
+
+        How about {number}
+
+        { "##" + "🥲" * number.value }
+        """)
+    return
 
 if __name__ == "__main__":
     app.run()
